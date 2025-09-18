28d10adfc3eaf1b288d9657582280a9eab4023df
Michael Lee <lee@soi2.org>
2025-09-18 09:37:34 -0500

    Updating list processing examples

--- src/l03_lists.ml
+++ src/l03_lists.ml
@@ -1,38 +1,66 @@
-  
 (* list processing *)
 
-let first (x :: _) = x
+(* basic pattern matching *)
 
-let first' lst =
-  match lst with
-  | (x :: _) -> x
-  | _ -> failwith "List is empty!"
+let first lst = match lst with
+  | [] -> failwith "Empty list!"
+  | x :: _ -> x
 
-let rest (x :: xs) = xs
+let rest lst = match lst with
+  | [] -> failwith "Empty list!"
+  | _ :: xs -> xs
 
-let second (_ :: x :: _) = x
-
-let third (_ :: _ :: x :: _) = x
-
+(* list processing functions *)
 
 let rec length = function
-  | [] -> 0
+  | [] -> 0 
   | _ :: xs -> 1 + length xs
 
-let rec sum = function
-  | [] -> 0
-  | x :: xs -> x + sum xs
+let rec index lst n =
+  if n = 0 then first lst
+  else index (rest lst) (n-1)
 
-let index n lst =
+let index' lst n =
   let rec aux i = function
-    | [] -> failwith "Index out of bounds"
-    | x :: xs -> if i = 0 then x else aux (i-1) xs
-  in aux n lst
+    | [] -> failwith "Empty list!"
+    | x :: xs -> if i = n then x
+                 else aux (i+1) xs
+  in aux 0 lst
 
-let rec append l1 l2 =
-  match l1 with
-  | [] -> l2
-  | x :: rest -> x :: append rest l2
+let rec append lst1 lst2 = match lst1 with
+  | [] -> lst2
+  | x :: xs -> x :: append xs lst2
 
-(* list generation *)
+let rec sum_list = function
+  | [] -> 0
+  | n :: ns -> n + sum_list ns
 
+let sum_list' lst =
+  let rec aux acc = function
+    | [] -> acc
+    | n :: ns -> aux (n + acc) ns 
+  in aux 0 lst
+
+let rec reverse = function
+  | [] -> []
+  | x :: xs -> append (reverse xs) [x]
+
+let reverse' lst =
+  let rec aux acc = function
+    | [] -> acc
+    | x :: xs -> aux (x :: acc) xs
+  in aux [] lst 
+
+(* list generating functions *)
+
+let rec range m n = if m > n then []
+                    else m :: range (m+1) n
+
+let rec repeat n x = if n = 0 then []
+                     else x :: repeat (n-1) x
+
+let fibs n =
+  let rec aux i j k acc =
+    if k = n then reverse acc
+    else aux j (i+j) (k+1) (i :: acc) 
+  in aux 1 1 0 []
