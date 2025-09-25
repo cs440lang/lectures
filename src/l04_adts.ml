a841ce520a71facfcbacc26f920170952ac4001d
Michael Lee <lee@soi2.org>
2025-09-24 22:31:44 -0500

    Cleaning up ADT examples

--- src/l04_adts.ml
+++ src/l04_adts.ml
@@ -12,6 +12,7 @@ let m : matrix = [[1;2;3];
 
 (* Algebraic Data Types - ADTs *)
 
+(* simple enumeration-like type *)
 type color = Red | Blue | Green 
 
 let describe_color = function
@@ -19,6 +20,7 @@ let describe_color = function
   | Blue-> "Cool"
   | Green -> "Verdant"
 
+(* constructors can carry additional data *)
 type box = Box of int
 
 let add_to_box i (Box j) = Box (i + j)
@@ -29,6 +31,7 @@ let add_to_box' i b = match b with
 let add_to_box'' i = function
   | Box j -> Box (i + j) 
 
+(* constructors can take different data *)
 type shape = Circle of float
            | Rectangle of float * float
            | Triangle of float * float
@@ -38,31 +41,29 @@ let shape_area = function
   | Rectangle (l,w) -> l *. w
   | Triangle (b,h) -> b *. h /. 2.0
 
-(* Recursive type *) 
-
-type russian_doll = Empty
+(* a recursive type *) 
+type russian_doll = EmptyDoll
                   | Doll of russian_doll
 
 let count_dolls d =
   let rec aux acc = function
-    | Empty -> acc
+    | EmptyDoll -> acc
     | Doll d' -> aux (acc + 1) d'
   in aux 0 d
 
-(* Recursive type carrying info *) 
+(* a data structure *)
+type int_list = INull
+              | ICons of int * int_list
 
-type int_list = Null
-              | Cons of int * int_list
-
-let l = Cons (1, Cons (2, Cons (3, Null)))
+let l = ICons (1, ICons (2, ICons (3, INull)))
 
 let rec sum_int_list = function
-  | Null -> 0
-  | Cons (x, l) -> x + sum_int_list l 
+  | INull -> 0
+  | ICons (x, l) -> x + sum_int_list l 
 
 let rec int_list_from_range m n =
-  if m = n then Null
-  else Cons (m, int_list_from_range (m+1) n) 
+  if m = n then INull
+  else ICons (m, int_list_from_range (m+1) n) 
 
 (* Polymorphic types *) 
 
@@ -70,8 +71,10 @@ type 'a pbox = PBox of 'a
 
 let unpack_pbox (PBox x) = x
 
+(* option is defined by OCaml!  *)
 type 'a option = None | Some of 'a
 
+(* we use the option type to support well-typed "null" *)
 let quadratic_roots a b c =
   let disc = (b *. b) -. (4. *. a *. c) in
   if disc < 0.0 then
@@ -82,15 +85,18 @@ let quadratic_roots a b c =
     let r2 = (-.b -. sqrt_disc) /. (2. *. a) in
     Some (r1, r2)
 
+(* polymorphic type with multiple type vars *)
 type ('a, 'b) either = Left of 'a | Right of 'b
 
 let k2f t = if t < 0.
             then Left "Invalid temp"
             else Right (t -. 272.15)
 
+(* our version of the built-in list *)
 type 'a my_list = Null
                 | Cons of 'a * 'a my_list
 
+(* a binary tree *)
 type ('k,'v) bin_tree = Nil
                       | Node of 'k * 'v
                                 * ('k,'v) bin_tree
