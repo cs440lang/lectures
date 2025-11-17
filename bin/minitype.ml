5f9bf6006bd125f17b83c0d437d14ce15fd78a33
Michael Lee <lee@soi2.org>
2025-11-17 15:31:52 -0600

    Adding executables for some REPLs

---
+++ bin/minitype.ml
@@ -0,0 +1,30 @@
+open L10_typeinfer
+
+let rec repl () =
+  print_string "> ";
+  flush stdout;
+  match read_line () with
+  | "" -> ()
+  | line -> (
+      try
+        let expr = Eval.parse line in
+        let inferred_type = Poly_typeinfer.infer expr in
+        let value = Eval.eval expr [] in
+        Printf.printf "- : %s = %s\n"
+          (Poly_typeinfer.string_of_type inferred_type)
+          (Eval.string_of_val value);
+        repl ()
+      with
+      | Poly_typeinfer.TypeError msg ->
+          Printf.printf "Type Error: %s\n" msg;
+          repl ()
+      | Eval.RuntimeError msg ->
+          Printf.printf "Runtime Error: %s\n" msg;
+          repl ()
+      | Parser.Error ->
+          print_endline "Parse error.";
+          repl ())
+
+let () =
+  print_endline "Type Inferred MiniML REPL:\n";
+  repl ()
