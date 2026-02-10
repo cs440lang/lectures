527232b88f3ee3a25c5c99333ab6d5f50a573ebc
Michael Lee <lee@soi2.org>
2026-02-09 18:14:20 -0600

    Updating lists source code

--- src/l05_lists.ml
+++ src/l05_lists.ml
@@ -1,84 +1,96 @@
 (* basic pattern matching *)
 
-let first l = match l with
-  | [] -> failwith "List is empty"
+let is_empty lst =
+  match lst with
+  | [] -> true
+  | _ -> false
+
+let head = function
+  | [] -> failwith "empty list"
   | x :: _ -> x
 
-let first' = function
-  | [] -> failwith "List is empty"
-  | x :: _ -> x
-
-let rest = function
-  | [] -> failwith "List is empty"
+let tail = function
+  | [] -> failwith "empty list"
   | _ :: xs -> xs
 
-(* processing lists *)
+let is_singleton = function
+  | [x] -> true
+  | _ -> false
 
-let rec sum = function
-  | [] -> 0
-  | x :: xs -> x + sum xs
+let sum_next_two = function
+  | _ :: x :: y :: _ -> x + y
+  | _ -> 0
+
+let first_of_first = function
+  | (x :: _) :: _ -> x
+  | _ -> failwith "bad structure"
+
+(* processing lists *)
 
 let rec length = function
   | [] -> 0
   | _ :: xs -> 1 + length xs
 
-(* tail recursive version *)
-let length' lst =
-  let rec aux acc = function 
-    | [] -> acc
-    | _ :: xs -> aux (1 + acc) xs in
-  aux 0 lst
+let rec sum = function
+  | [] -> 0
+  | x :: xs -> x + sum xs
 
-let rec index n = function
-  | [] -> failwith "Index out of range"
-  | x :: xs -> if n = 0 then x
-               else index (n-1) xs
+(* building lists *)
 
-let rec index' n = function
-  | [] -> failwith "Index out of range"
-  | x :: xs when n = 0 -> x
-  | _ :: xs -> index' (n-1) xs
+let rec range n =
+  if n <= 0 then []
+  else n :: range (n - 1)
 
-let rec append lst1 lst2 = match lst1 with
+(* processing & building lists *)
+
+let rec double = function
+  | [] -> []
+  | x :: xs -> (2 * x) :: double xs
+
+let rec map f = function
+  | [] -> []
+  | x :: xs -> f x :: map f xs
+
+let rec filter p = function
+  | [] -> []
+  | x :: xs when p x -> x :: filter p xs
+  | _ :: xs -> filter p xs
+
+(* concatenating lists *)
+
+let rec append lst1 lst2 =
+  match lst1 with
   | [] -> lst2
   | x :: xs -> x :: append xs lst2
- 
-(* poor runtime complexity -- O(n^2) *)
+
+(* inefficient reverse - O(N^2) *)
+
 let rec reverse = function
   | [] -> []
-  | x :: xs -> append (reverse xs) [x]
+  | x :: xs -> reverse xs @ [x]
 
-(* much more efficient with an accumulator *)
-let reverse' lst =
+(* accumulator-driven, tail-recursive reverse - O(N) *)
+
+let rec reverse' lst acc =
+  match lst with
+  | [] -> acc
+  | x :: xs -> reverse' xs (x :: acc)
+
+(* tail-recursive version with helper *)
+
+let reverse'' lst =
   let rec aux acc = function
-     | [] -> acc
-     | x :: xs -> aux (x :: acc) xs in
+    | [] -> acc
+    | x :: xs -> aux (x :: acc) xs in
   aux [] lst
 
-(* list generating functions *)
+(* tail-recursive map *)
 
-let rec range m n = if m > n then []
-                    else m :: range (m+1) n
-
-let rec repeat n x = if n = 0 then []
-                     else x :: repeat (n-1) x
-
-let fibs n =
-  let rec fib = function
-    | 0 -> 1
-    | 1 -> 1
-    | i -> fib (i-1) + fib (i-2)
-  and gen j = if j=n
-              then []
-              else fib j :: gen (j+1)
-  in
-  gen 0
-
-let fibs' n =
-  let rec gen i j k acc =
-    if k = n then reverse acc
-    else gen j (i+j) (k+1) (i :: acc) in
-  gen 1 1 0 []
+let map' f lst =
+  let rec aux acc = function
+    | [] -> List.rev acc (* reverse the accumulated result *)
+    | x :: xs -> aux (f x :: acc) xs in
+  aux [] lst
 
 (* associative lists *)
 
@@ -86,3 +98,9 @@ let rec assoc k = function
   | [] -> raise Not_found
   | (k',v) :: xs when k = k' -> v
   | _ :: xs -> assoc k xs 
+
+let mem_assoc k alst = 
+  try
+    let _ = assoc k alst in true
+  with
+  | Not_found -> false
