791d33c3e94a349d6fa7d911189e39930e5db1f9
Michael Lee <lee@soi2.org>
2026-02-10 18:35:20 -0600

    WIP ADT lecture source

--- src/l06_adts.ml
+++ src/l06_adts.ml
@@ -1,23 +1,58 @@
+type suit = Diamonds | Clubs | Hearts | Spades
+
+let card: int * suit = (10, Hearts)
+
+let int_of_suit : suit -> int = function
+  | Spades -> 4
+  | Hearts -> 3
+  | Clubs -> 2
+  | Diamonds -> 1
+
+
 (* Tuples ********************************************************************)
-
-let pt : float * float = (2.17, 3.14)
-
-let weird : (int * string) * int list = ((42, "hi"), [1;2;3])
 
 let dist (x1,y1) (x2,y2) = sqrt ((x1-.x2)**2. +. (y1-.y2)**2.)
 
+let swap (x,y) = (y,x)
+
+let inc_and_print x =
+  let () = print_string "x = " in
+  let () = print_int x in
+  let () = print_newline () in
+  x + 1
+
+let inc_and_print' x =
+  let () = Format.printf "x = %d\n" x in
+  x + 1
+
+let inc_and_print'' x =
+  print_string "x = ";
+  print_int x;
+  print_newline ();
+  x + 1
+
 (* records *)
 
-type student = { name : string; id : int; gpa : float; }
+type student = { name: string; id: int; gpa: float; }
 
 let michael : student = { name="Michael"; id=1234567; gpa=3.5 }
 
+let jane = { michael with name="Jane"; id=2345678 }
+
+
 type 'a list_node = { data : 'a; next : 'a list_node }
 
-let rec ll : string list_node = { data="lions";
-                                  next={ data="tigers";
-                                         next={ data="bears";
-                                                next=ll }}}
+let rec ll : string list_node = { data="lions"; next={
+                                  data="tigers"; next={
+                                  data="bears"; next=ll }}}
+
+let traverse lst =
+  let rec aux { data; next; } =
+    print_endline data;
+    if next == lst then ()
+    else aux next in
+  aux lst
+
 
 (* Type Synonyms *************************************************************)
 
@@ -39,26 +74,12 @@ let m' : float matrix = [[1.;2.;3.];
 
 (* Algebraic Data Types - ADTs ***********************************************)
 
-(* simple enumeration-like type *)
-type color = Red | Blue | Green 
+type suit = Diamonds | Clubs | Hearts | Spades
 
-let describe_color = function
-  | Red -> "Warm"
-  | Blue-> "Cool"
-  | Green -> "Verdant"
+type rank = Num of int | Jack | Queen | King | Ace
 
-(* constructors can carry additional data *)
-type box = Box of int
+type card = rank * suit
 
-let add_to_box i (Box j) = Box (i + j)
-
-let add_to_box' i b = match b with
-  | Box j -> Box (i + j) 
-
-let add_to_box'' i = function
-  | Box j -> Box (i + j) 
-
-(* constructors can take different data *)
 type shape = Circle of float
            | Rectangle of float * float
            | Triangle of float * float
@@ -68,35 +89,8 @@ let shape_area = function
   | Rectangle (l,w) -> l *. w
   | Triangle (b,h) -> b *. h /. 2.0
 
-(* a recursive type *) 
-type russian_doll = EmptyDoll
-                  | Doll of russian_doll
-
-let count_dolls d =
-  let rec aux acc = function
-    | EmptyDoll -> acc
-    | Doll d' -> aux (acc + 1) d' in
-  aux 0 d
-
-(* a data structure *)
-type int_list = Null
-              | Cons of int * int_list
-
-let l = Cons (1, Cons (2, Cons (3, Null)))
-
-let rec sum_int_list = function
-  | Null -> 0
-  | Cons (x, l) -> x + sum_int_list l 
-
-let rec int_list_from_range m n =
-  if m = n then Null
-  else Cons (m, int_list_from_range (m+1) n) 
 
 (* Polymorphic types *********************************************************) 
-
-type 'a pbox = PBox of 'a
-
-let unpack_pbox (PBox x) = x
 
 (* option is defined by OCaml!  *)
 type 'a option = None | Some of 'a
@@ -116,15 +110,9 @@ let rec assoc_opt k = function
   | (k',v) :: xs when k = k' -> Some v
   | _ :: xs -> assoc_opt k xs 
 
-(* polymorphic type with multiple type vars *)
-type ('a, 'b) either = Left of 'a | Right of 'b
-
-let k2f t = if t < 0. then Left "Invalid temp"
-            else Right (t -. 272.15)
-
 (* our version of the built-in list *)
-type 'a my_list = Null'
-                | Cons' of 'a * 'a my_list
+type 'a my_list = Null
+                | Cons of 'a * 'a my_list
 
 (* a binary tree *)
 type ('k,'v) bin_tree = Nil
