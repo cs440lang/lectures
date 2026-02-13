51617a23aa34b394b5cef7557c766e63a17b7b1c
Michael Lee <lee@soi2.org>
2026-02-12 23:57:13 -0600

    Merging in main

--- src/l05_lists.ml
+++ src/l05_lists.ml
@@ -1,37 +1,106 @@
 (* basic pattern matching *)
 
-let first lst =
-  failwith "Unimplemented"
+let is_empty lst =
+  match lst with
+  | [] -> true
+  | _ -> false
+
+let head = function
+  | [] -> failwith "empty list"
+  | x :: _ -> x
+
+let tail = function
+  | [] -> failwith "empty list"
+  | _ :: xs -> xs
+
+let is_singleton = function
+  | [x] -> true
+  | _ -> false
+
+let sum_next_two = function
+  | _ :: x :: y :: _ -> x + y
+  | _ -> 0
+
+let first_of_first = function
+  | (x :: _) :: _ -> x
+  | _ -> failwith "bad structure"
 
 (* processing lists *)
 
-let rec sum lst =
-  failwith "Unimplemented"
+let rec length = function
+  | [] -> 0
+  | _ :: xs -> 1 + length xs
 
-let rec length lst =
-  failwith "Unimplemented"
+let rec sum = function
+  | [] -> 0
+  | x :: xs -> x + sum xs
 
-let rec index n lst =
-  failwith "Unimplemented"
+(* building lists *)
+
+let rec range n =
+  if n <= 0 then []
+  else n :: range (n - 1)
+
+(* processing & building lists *)
+
+let rec double = function
+  | [] -> []
+  | x :: xs -> (2 * x) :: double xs
+
+let rec map f = function
+  | [] -> []
+  | x :: xs -> f x :: map f xs
+
+let rec filter p = function
+  | [] -> []
+  | x :: xs when p x -> x :: filter p xs
+  | _ :: xs -> filter p xs
+
+(* concatenating lists *)
 
 let rec append lst1 lst2 =
-  failwith "Unimplemented"
- 
-let reverse lst =
-  failwith "Unimplemented"
+  match lst1 with
+  | [] -> lst2
+  | x :: xs -> x :: append xs lst2
 
-(* list generating functions *)
+(* inefficient reverse - O(N^2) *)
 
-let rec range m n =
-  failwith "Unimplemented"
+let rec reverse = function
+  | [] -> []
+  | x :: xs -> reverse xs @ [x]
 
-let rec repeat n x =
-  failwith "Unimplemented"
+(* accumulator-driven, tail-recursive reverse - O(N) *)
 
-let fibs n =
-  failwith "Unimplemented"
+let rec reverse' lst acc =
+  match lst with
+  | [] -> acc
+  | x :: xs -> reverse' xs (x :: acc)
+
+(* tail-recursive version with helper *)
+
+let reverse'' lst =
+  let rec aux acc = function
+    | [] -> acc
+    | x :: xs -> aux (x :: acc) xs in
+  aux [] lst
+
+(* tail-recursive map *)
+
+let map' f lst =
+  let rec aux acc = function
+    | [] -> List.rev acc (* reverse the accumulated result *)
+    | x :: xs -> aux (f x :: acc) xs in
+  aux [] lst
 
 (* associative lists *)
 
-let rec assoc k alst =
-  failwith "Unimplemented"
+let rec assoc k = function
+  | [] -> raise Not_found
+  | (k',v) :: xs when k = k' -> v
+  | _ :: xs -> assoc k xs 
+
+let mem_assoc k alst = 
+  try
+    let _ = assoc k alst in true
+  with
+  | Not_found -> false
